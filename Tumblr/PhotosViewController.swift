1e8e40e12ce489b34f59b59fdb68d1c63599ed0c
fionabronwen <fionabronwen@gmail.com>
2017-02-09 18:40:49 -0500

    Infinite scroll implementation

--- Tumblr/PhotosViewController.swift
+++ Tumblr/PhotosViewController.swift
@@ -13,19 +13,23 @@ class PhotosViewController: UIViewController, UITableViewDelegate, UITableViewDa
 
     var posts: [NSDictionary] = []
     var isMoreDataLoading = false
+    var refreshControl = UIRefreshControl()
+    var offsetCount = 0
     
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         tableView.delegate = self
         tableView.dataSource = self
         
-        let refreshControl = UIRefreshControl()
-        refreshControl.addTarget(self, action: #selector(refreshControlAction(_:)), for: UIControlEvents.valueChanged)
+        refreshControl.addTarget(self, action: #selector(refreshControlAction), for: UIControlEvents.valueChanged)
         
-        refreshControlAction(refreshControl)
+        fetchPosts(sender: "viewDidLoad")
         
         tableView.insertSubview(refreshControl, at: 0)
+        
+        
         
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -34,10 +38,22 @@ class PhotosViewController: UIViewController, UITableViewDelegate, UITableViewDa
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
     
-    func refreshControlAction(_ refreshControl: UIRefreshControl) {
+    func refreshControlAction() {
+        fetchPosts(sender: "refreshControl")
+    }
+    
+    func fetchPosts(sender: String) {
         
-        let url = URL(string:"https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")
-        let request = URLRequest(url: url!)
+        var url: URL
+        
+        if sender == "scrollViewDidScroll" {
+            url = URL(string:"https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?offset=\(offsetCount)&api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
+        }
+        else {
+            url = URL(string:"https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
+            
+        }
+        let request = URLRequest(url: url)
         let session = URLSession(
             configuration: URLSessionConfiguration.default,
             delegate:nil,
@@ -55,14 +71,19 @@ class PhotosViewController: UIViewController, UITableViewDelegate, UITableViewDa
                         // Recall there are two fields in the response dictionary, 'meta' and 'response'.
                         // This is how we get the 'response' field
                         let responseFieldDictionary = responseDictionary["response"] as! NSDictionary
-                        self.posts = responseFieldDictionary["posts"] as! [NSDictionary]
-                        
+                        if sender == "refreshControl"{
+                            self.posts = responseFieldDictionary["posts"] as! [NSDictionary]
+
+                        }else{
+                            self.posts += responseFieldDictionary["posts"] as! [NSDictionary]
+                        }
                         
                         // This is where you will store the returned array of posts in your posts property
                         // self.feeds = responseFieldDictionary["posts"] as! [NSDictionary]
                     }
                     self.tableView.reloadData()
-                    refreshControl.endRefreshing()
+                    self.refreshControl.endRefreshing()
+                    self.isMoreDataLoading = false
                 }
         });
         task.resume()
@@ -106,14 +127,18 @@ class PhotosViewController: UIViewController, UITableViewDelegate, UITableViewDa
 
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         // Handle scroll behavior here
+        
         if (!isMoreDataLoading) {
             let scrollViewContentHeight = tableView.contentSize.height
             let scrollOffsetThreshold = scrollViewContentHeight - tableView.bounds.size.height
             
             if(scrollView.contentOffset.y > scrollOffsetThreshold && tableView.isDragging) {
                 isMoreDataLoading = true
+                offsetCount += 20
+                fetchPosts(sender: "scrollViewDidScroll")
             }
         }
+        
     }
 
     
